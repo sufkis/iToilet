8f2c08451ce22d87784f9569a73f279d36cb47b1
sufkis <asafsmirnov@gmail.com>
2021-05-12 16:20:54 +0300

    Add firebase authentication
    Added some inputs and a signup button which will add the information to firebase authentication.

---
+++ client/src/contexts/Auth.js
@@ -0,0 +1,56 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { auth, provider } from '../firebase'
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true);
+
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    function googleSignIn() {
+        return auth.signInWithPopup(provider);
+    }
+
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password);
+    }
+
+    function logout() {
+        return auth.signOut();
+    }
+
+    useEffect(() => {
+        let isMounted = true;
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            isMounted && setCurrentUser(user);
+            isMounted && setLoading(false);
+        })
+
+        return () => {
+            isMounted = false; 
+            unsubscribe();
+        }
+    }, [])
+
+    const context = {
+        currentUser,
+        signup,
+        googleSignIn,
+        login,
+        logout,
+    }
+
+    return (
+        <AuthContext.Provider value={context}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}

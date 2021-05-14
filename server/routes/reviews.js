12b89eb64e12aba6e075f663dd75efb020a29d39
Juli-h <guliag4@gmail.com>
2021-05-14 17:42:33 +0300

    controller folder

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,71 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const reviewService = require('../services/reviews'); // business logic
+const controller = require('../controllers/reviews');
 
 
-router.post('/', async (req, res) => {
-  try{
-    await reviewService.createReview(req.body);
-    res.send();
-  }
-  catch(err){
-    console.log(err);
-    res.status(500).send(err);
-  }
-});
-
-router.put('/:reviewId', async (req, res) => {
-  const { text, rate } = req.body;
-  try {
-    await reviewService.updateReview( req.params.reviewId, text, rate );
-    res.send();
-  }
-  catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-})
-
-router.get('/', async (req, res) => {   // https://localhost:5000/reviews?userId=123&toiletId=321
-  try{
-    const results = await reviewService.getReviewsByQuery(req.query);
-    res.send(results);
-  }
-  catch(err){
-    console.log(err);
-    res.status(500).send(err);
-  }
-});
-
-router.get('/:reviewId', async (req, res) => {
-  try {
-    const result = await reviewService.getOneReview(req.params.reviewId);
-    res.send(result);
-  }
-  catch(err){
-    console.log(err);
-    res.status(500).send(err);
-  }
-})
-
-router.delete('/:reviewId', async (req, res) => {
-  try {
-    await reviewService.deleteReview(req.params.reviewId);
-    res.send({ message: 'deleted successfully' });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  } 
-});
-
-router.delete('/', async (req, res) => {
-  try {
-    await reviewService.deleteAll();
-    res.send({ message: 'deleted successfully' });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  } 
-});
+router.post('/', controller.createReview);
+router.put('/:reviewId', controller.updateReview)
+router.get('/', controller.getReviewsByQuery);
+router.get('/:reviewId', controller.getOneReview);
+router.delete('/:reviewId', controller.deleteReview);
+router.delete('/', controller.deleteAll);
 
 module.exports = router;

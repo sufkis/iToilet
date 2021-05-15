6283a670c6d5de8ca9117de2da70c8aa7563364f
Jonathan Spandorf <jonspandorf@gmail.com>
2021-05-15 21:38:43 +0300

    fixing conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,27 +8,10 @@ function App() {
 // useAuth to deconstruct: currentUser, googleSignIn, login, logout from useAuth() hook
   // currentUser is null if no one is logged in, and signup also logs you in.
 
-  const AppRouter = () => {
-
-    return (
-      <Router>
-        <Switch>
-          <Route exact path='/signup'>
-            <Signup />
-          </Route>
-          <Route exact path='/'>
-            {/* Map Component here */}
-          </Route>
-        </Switch>
-      </Router>
-    )
-  }
 
   return (
     <>
-    <AuthProvider>
-      <AppRouter />
-    </AuthProvider>
+    <Signup />
     </>
   );
 }

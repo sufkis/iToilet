26ed06fa41e5b312b042d6ef7614b462c6cbc989
Jonathan Spandorf <jonspandorf@gmail.com>
2021-05-16 13:10:57 +0300

    added mock toilet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import './App.css';
-import Signup from './components/signup';
+// import Signup from './components/signup';
 import { AuthProvider } from "./contexts/Auth"
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Map from './Map';
+import toiletIcon from "./toilet.png";
+
+
+const mockToilet = [
+  {lat: 32.081902599570284, lng: 34.78146508564525, text: "Ibn Gvirol 69", href: {toiletIcon}, isPublic: true},
+  {lat: 32.069861139032405, lng: 34.771061427974495, text: "Kikar magen David", href: {toiletIcon}, isPublic: true},
+  {lat: 32.08498129236291, lng: 34.78301725127104, text: "Lotte Reich Community Center", href: {toiletIcon}, isPublic: true},
+  {lat: 32.079500727881204, lng: 34.773777057720466, text: "Passageway Dizingoff & Frishman", href: {toiletIcon}, isPublic: true},
+  {lat: 32.066856524717885, lng: 34.768301693600314, text: "Beit Tami Community Center", href: {toiletIcon}, isPublic: true},
+  {lat: 32.07763255283553, lng: 34.7788800582799, text: "Nam Restaurant", href: {toiletIcon}, isPublic: false},
+  {lat: 32.061972973398, lng: 34.77088098627881, text: "Herzl 16", href: {toiletIcon}, isPublic: false},
+  {lat: 32.056336509619854, lng: 34.768404369301386, text: "Anita florentine", href: {toiletIcon}, isPublic: false},
+  {lat: 32.053360328714675, lng: 34.75639650165063, text: "Shaffa Bar" ,href: {toiletIcon}, isPublic: false},
+]
+
 
 function App() {
 
@@ -15,10 +30,10 @@ function App() {
       <Router>
         <Switch>
           <Route exact path='/signup'>
-            <Signup />
+            {/* <Signup /> */}
           </Route>
           <Route exact path='/'>
-            <Map />
+            <Map toilets={mockToilet}/>
           </Route>
         </Switch>
       </Router>
--- client/src/Map.js
+++ client/src/Map.js
@@ -2,27 +2,32 @@ import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import toiletIcon from "./toilet.png";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => < div > { text } < /div>;
 
 function Map() {
-  const [lat, setLat] = useState(32.06342);
-  const [lng, setLng] = useState(34.773181);
-  return (
-    <div style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: `AIzaSyDqrYGToxFX3WcTkH6Dpllx7F0JnSwjuOM` }}
-        defaultCenter={{ lat: lat, lng: lng }}
-        defaultZoom={15}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
-          href={toiletIcon}
-        />
-      </GoogleMapReact>
-    </div>
-  );
+    const [lat, setLat] = useState(32.06342);
+    const [lng, setLng] = useState(34.773181);
+    return ( <
+        div style = {
+            { height: "75%", width: "100%" }
+        } >
+        <
+        GoogleMapReact bootstrapURLKeys = {
+            { key: `AIzaSyDqrYGToxFX3WcTkH6Dpllx7F0JnSwjuOM` }
+        }
+        defaultCenter = {
+            { lat: lat, lng: lng }
+        }
+        defaultZoom = { 15 } >
+        <
+        AnyReactComponent lat = { 59.955413 }
+        lng = { 30.337844 }
+        text = "My Marker"
+        href = { toiletIcon }
+        /> < /
+        GoogleMapReact > <
+        /div>
+    );
 }
 
 export default Map;

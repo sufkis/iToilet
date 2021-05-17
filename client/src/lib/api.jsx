80efc9c0ca26bb60970c0d1844fa5e90a4f9e375
Jonathan Spandorf <jonspandorf@gmail.com>
2021-05-17 22:27:29 +0300

    added api library

--- client/src/lib/api.jsx
+++ client/src/lib/api.jsx
@@ -1,6 +1,23 @@
 import axios from 'axios';
 
+const BaseUrl = `http://127.0.0.1:5000/`
+
+// GOOGLE MAPS API FUNCTIONS
+
 export async function convertToGoogleCeooding(url) {
     const response = await axios.get(url);
     return response.data;
 }
+
+// CUSTOMER FUNCTIONS
+
+export async function getToilets() {
+    // TODO
+}
+
+// PROVIDER FUNCTION
+
+export async function postToilet(form) {
+    // TODO
+}
+

ca367cf3ea915d8c9c66b5da1c4fa002b374f288
Jonathan Spandorf <jonspandorf@gmail.com>
2021-05-17 23:15:04 +0300

    added login and navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,25 @@ import './App.css';
 import Signup from './components/signup';
 import { AuthProvider } from "./contexts/Auth"
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Login from './components/login';
+import Navigation from './components/navbar';
 
 function App() {
 
 // useAuth to deconstruct: currentUser, googleSignIn, login, logout from useAuth() hook
-  // currentUser is null if no one is logged in, and signup also logs you in.
+  // currentUser is null if no one is logged in, and signup also logs you in. 
 
   const AppRouter = () => {
 
     return (
       <Router>
+        <Navigation />
         <Switch>
           <Route exact path='/signup'>
             <Signup />
+          </Route>
+          <Route exact path='/login'>
+            <Login />
           </Route>
           <Route exact path='/'>
             {/* Map Component here */}
---
+++ client/src/components/login.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react"
+import { useHistory } from "react-router";
+import { useAuth } from "../contexts/Auth";
+
+
+const Login = () => {
+
+    const { login, googleSignIn } = useAuth();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const history = useHistory();
+
+    async function handleGoogleSignIn () {
+        try {
+            await googleSignIn()
+            history.push('/');
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await login(email, password);
+            history.push('/')
+        } catch(err) {
+            console.error(err)
+        }
+    }
+
+    return(
+        <>
+        <div className="container w-50">
+            <h1>Login</h1>
+            <form onSubmit={handleLogin} className="form-group">
+                <label htmlFor="email">Email: </label>
+                <input 
+                className="form-control mt-1"
+                type="email"
+                value={email}
+                onChange={e=> setEmail(e.target.value)}
+                />
+                <label htmlFor="email">Password: </label>
+                <input 
+                className="form-control mt-1"
+                type="email"
+                value={password}
+                onChange={e=> setPassword(e.target.value)}
+                />
+                <div className="d-flex justify-content-center mt-2">
+                    <button type="submit" className="btn btn-primary">Login</button>
+                </div>
+            </form>
+            <div className="d-flex justify-content-center mt-2">
+                <button className="btn btn-danger" type="submit" onClick={handleGoogleSignIn}>Sign In with Google account</button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Login
